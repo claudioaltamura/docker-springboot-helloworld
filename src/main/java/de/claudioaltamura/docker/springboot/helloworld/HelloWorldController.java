45a7fc4a00db8f4bd8113793777f1d407d150ea6
Claudio Altamura <claudio@claudioaltamura.de>
2019-01-05 09:28:55 +0100

    new: added logging

--- src/main/java/de/claudioaltamura/docker/springboot/helloworld/HelloWorldController.java
+++ src/main/java/de/claudioaltamura/docker/springboot/helloworld/HelloWorldController.java
@@ -1,6 +1,8 @@
 package de.claudioaltamura.docker.springboot.helloworld;
 
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+  private static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
+  
   @GetMapping(value = {"/helloworld", "/helloworld/{name}"})
   public String greet(@PathVariable final Optional<String> name) {
+    log.debug("greet '{}'", name.isPresent() ?  name.get() : "");
+    
     if (name.isPresent())
       return "Hello " + name.get() + "!";
     else {
